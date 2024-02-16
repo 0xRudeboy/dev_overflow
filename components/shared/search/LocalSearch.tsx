3305c3698ff27d813b9a2f34396190e55c59b5fa
0xRUDEBOY <116237237+0xRUDEBOY@users.noreply.github.com>
2024-02-16 14:26:11 +0400

    finished  side bars and ask questions and a whole lot

---
+++ components/shared/search/LocalSearch.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React from "react";
+
+interface CustomInputProps {
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({
+  route,
+  iconPosition,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: CustomInputProps) => {
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          alt="search-icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      )}
+
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value=""
+        onChange={() => {}}
+        className="paragraph-regular no-focus placeholder background-light800_darkgradient text-dark400_light700 border-none shadow-none outline-none"
+      />
+
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          alt="search-icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      )}
+    </div>
+  );
+};
+
+export default LocalSearch;
+
+// "flex h-9 w-full rounded-md border border-slate-200 bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -13,18 +13,25 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.remove("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
     handleThemeChange();
   }, [mode]);
+  // removed the "mode" variable from dependency array to avoid infinite loop
+
+  // console.log("mode", mode);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
