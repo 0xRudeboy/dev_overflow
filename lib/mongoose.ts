4139d968dae38202d7c3f15a1ff30be16b7d6caf
0xRUDEBOY <116237237+0xRUDEBOY@users.noreply.github.com>
2024-02-17 20:10:41 +0400

    connected MongoDb + completed questions reading from db

---
+++ lib/mongoose.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) {
+    return console.log("MISSING MONGODB_URL");
+  }
+
+  if (isConnected) {
+    return console.log("MongoDb is already connected");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "devflow",
+    });
+
+    isConnected = true;
+
+    console.log("MongoDb is connected");
+  } catch (e) {
+    console.log("MongoDb connection error", e);
+  }
+};
