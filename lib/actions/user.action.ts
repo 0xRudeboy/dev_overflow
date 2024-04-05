40c6fcc14f7e2a6879a0da191419497ae7ffa3d6
0xRUDEBOY <116237237+0xRUDEBOY@users.noreply.github.com>
2024-04-06 03:35:10 +0400

    Bug fix: comment out unused variables in user.actions.ts

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -149,7 +149,9 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+    // const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+
+    const { clerkId, searchQuery } = params;
 
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, "i") } }
