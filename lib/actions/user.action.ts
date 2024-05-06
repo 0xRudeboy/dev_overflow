cf3cb96cd81e20142b16b20f9a582c75ccd355e3
0xRUDEBOY <116237237+0xRUDEBOY@users.noreply.github.com>
2024-05-06 21:33:11 +0400

    unused variable warning cleanup

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -217,7 +217,7 @@ export async function getUserQuestions(params: GetUserStatsParams) {
   try {
     connectToDatabase();
 
-    const { userId, page = 1, pageSize = 10 } = params;
+    const { userId } = params;
 
     const totalQuestions = await Question.countDocuments({ author: userId });
 
@@ -240,7 +240,7 @@ export async function getUserAnswers(params: GetUserStatsParams) {
   try {
     connectToDatabase();
 
-    const { userId, page = 1, pageSize = 10 } = params;
+    const { userId } = params;
 
     const totalAnswers = await Answer.countDocuments({ author: userId });
 
